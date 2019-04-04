004be62c51fbcadb8f39913b78443b412bbcce8b
huyaolong <huyaolong@01zhuanche.com>
2019-04-04 15:20:20 +0800

    init

---
+++ centos/login_show.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "-------------------------------------------------------"
+myUser=${USER}
+myHost=${HOSTNAME}
+echo $myUser@$myHost
+distro=$(cat /etc/redhat-release)
+echo "OS: $distro"
+echo "Kernel: $(uname -m ) $(uname -sr)"
+cpu_py_num=$(grep "physical id" /proc/cpuinfo | sort  | uniq | wc -l)
+cpu_vt_num=$(grep "physical id" /proc/cpuinfo | wc -l)
+cpu_model=$(grep "model name" /proc/cpuinfo | sort | uniq | awk -F : '{print $2}')
+echo CPU: $cpu_model \* $cpu_py_num
+echo CPU_CORES: $cpu_vt_num
+echo "-------------------------------------------------------"
+df -h
+echo "-------------------------------------------------------"
+free -g | head -n2
+echo "-------------------------------------------------------"
+who -b
+w
+echo "-------------------------------------------------------"
+lastlog | grep -v "\*\*"
+echo "-------------------------------------------------------"
+ss -s | grep ports
+echo "-------------------------------------------------------"
+ss -ntl
+echo "-------------------------------------------------------"
